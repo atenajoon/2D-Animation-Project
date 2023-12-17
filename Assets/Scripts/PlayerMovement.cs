93161b8457c67a4348afe18a95daf50ed799e0d8
Atena Razm <atenarazm@gmail.com>
2023-12-16 22:05:43 -0800

    character movement is smooth now

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
-    private Vector2 _movementInput;
+    private Vector2 _movementInput; // target position
+    private Vector2 _smoothedMovementInput; // current position
+    private Vector2 _movementInputSmoothedVelocity; // current velocity, which is modified every time the function is called
     [SerializeField] private float _speed;
     private void Awake()
     {
@@ -14,7 +16,9 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        _rigidbody.velocity = _movementInput * _speed;
+        // Vector2.SmoothDamp(currentPosition, targetPosition, ref currentVelocity, smoothTime);
+        _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _movementInputSmoothedVelocity, 0.1f);
+        _rigidbody.velocity = _smoothedMovementInput * _speed;
     }
     private void OnMove(InputValue inputValue)
     {
