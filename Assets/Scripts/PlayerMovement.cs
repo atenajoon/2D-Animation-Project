6837b27477b1062c8ffce8d995f1f817b757c1bf
Atena Razm <atenarazm@gmail.com>
2023-12-18 12:37:17 -0800

    old script, with smoothing commented out

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,20 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput; // target position
-    private Vector2 _smoothedMovementInput; // current position
-    private Vector2 _movementInputSmoothedVelocity; // current velocity, which is modified every time the function is called
+    // private Vector2 _smoothedMovementInput; // current position
+    // private Vector2 _movementInputSmoothedVelocity; // current velocity, which is modified every time the function is called
     [SerializeField] private float _speed;
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        // _rigidbody.bodyType = RigidbodyType2D.Dynamic;
     }
     private void FixedUpdate()
     {
         // Vector2.SmoothDamp(currentPosition, targetPosition, ref currentVelocity, smoothTime);
-        _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _movementInputSmoothedVelocity, 0.1f);
-        _rigidbody.velocity = _smoothedMovementInput * _speed;
+        // _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _movementInputSmoothedVelocity, 0.1f);
+        // _rigidbody.velocity = _smoothedMovementInput * _speed;
+        _rigidbody.velocity = _movementInput * _speed;
     }
     private void OnMove(InputValue inputValue)
     {
