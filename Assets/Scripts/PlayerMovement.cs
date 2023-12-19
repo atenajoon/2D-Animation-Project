e2e600fa1cb9510c9b4d162c5d2c178ca96cb1b4
Atena Razm <atenarazm@gmail.com>
2023-12-18 17:55:41 -0800

    Player moves and jumps if is on the ground

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,62 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
-    private Vector2 _movementInput; // target position
+    // private Vector2 _movementInput; // target position
     // private Vector2 _smoothedMovementInput; // current position
     // private Vector2 _movementInputSmoothedVelocity; // current velocity, which is modified every time the function is called
-    [SerializeField] private float _speed;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpForce;
+    private bool _isJumping;
+    private float _moveHorizontal;
+    private float _moveVertical;
+
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        // _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+    }
+        void Start()
+    {
+        _isJumping = false;
+    }
+
+    private void Update()
+    {
+        // this gives us a number (-1 or 1) on keypress to indicate moving directions
+        _moveHorizontal = Input.GetAxisRaw("Horizontal");
+        _moveVertical = Input.GetAxisRaw("Vertical");
     }
     private void FixedUpdate()
     {
+        // Rigidbody.AddForce(Vector2 Force for x & y axis, ForceMode);
+        if(_moveHorizontal > 0 || _moveHorizontal < 0)
+        {
+            _rigidbody.AddForce(new Vector2(_moveHorizontal * _moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+
+        if(!_isJumping && _moveVertical > 0)
+        {
+            _rigidbody.AddForce(new Vector2(0f, _moveVertical * _jumpForce), ForceMode2D.Impulse);  
+        }
         // Vector2.SmoothDamp(currentPosition, targetPosition, ref currentVelocity, smoothTime);
         // _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _movementInputSmoothedVelocity, 0.1f);
-        // _rigidbody.velocity = _smoothedMovementInput * _speed;
-        _rigidbody.velocity = _movementInput * _speed;
+        // _rigidbody.velocity = _smoothedMovementInput * _moveSpeed;
+        // _rigidbody.velocity = _movementInput * _moveSpeed;
     }
-    private void OnMove(InputValue inputValue)
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        _movementInput = inputValue.Get<Vector2>();
+        if(collision.gameObject.tag == "Platform")
+        {
+            _isJumping = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            _isJumping = true;
+        }
     }
 }
