6dbbcbeda710f45bf372fd868d1e222998359b75
Atena Razm <atenarazm@gmail.com>
2024-01-24 13:12:19 -0800

    enemy moves horizontally, needs some lag bug fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,51 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float moveSpeed;
+    public Vector2 moveOffset;
+    private Vector2 aPos;
+    private Vector2 bPos;
     public int health = 10;
     // public GameObject deathEffect;
+
+    void Start()
+    {
+        aPos = transform.position;
+        bPos = aPos;
+    }
+
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    private void MoveEnemy()
+    {
+
+        // moving the gameObject's position towards the bPos at the specified speed
+        Vector2 newPosition = Vector2.MoveTowards(transform.position, bPos, moveSpeed * Time.deltaTime);
+
+        // Smoothly moving the Rigidbody of the gameObject to the newPosition
+        GetComponent<Rigidbody2D>().MovePosition(newPosition);
+
+        // converting Vector3 Transform's position into a Vector2 by using the (Vector2) type casting operation,
+        // the result is stored in a a Vector2 variable;
+        Vector2 position2D = (Vector2)transform.position;
+
+        // in Vector2, for comparing floating-point numbers we don't use direct equality (==) due to the potential for very small differences.
+        // so, I compare postion2D and bPos to see if they are very close to each other within a small threshold
+        if (Vector2.Distance(position2D, bPos) < 0.1f)
+        {       
+            if (bPos == aPos)
+            {
+                bPos = aPos + moveOffset;
+            }
+            else
+            {
+                bPos = aPos;
+            }
+        }
+    }
 
     public void TakeDamage(int damage)
     {
