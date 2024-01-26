3cef4f253e5dad55fd320e54d6ea2154033af869
Atena Razm <atenarazm@gmail.com>
2024-01-25 18:42:57 -0800

    enemy movement approach changed to transform.Translate and MoveRange check

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,48 +5,37 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float moveSpeed;
-    public Vector2 moveOffset;
-    private Vector2 aPos;
-    private Vector2 bPos;
+    public float moveRange;
+    private bool movingRight = true;
+    private Vector2 originalPosition;
     public int health = 10;
-    // public GameObject deathEffect;
+
 
     void Start()
     {
-        aPos = transform.position;
-        bPos = aPos;
+        originalPosition = transform.position;
     }
+
 
     void Update()
     {
         MoveEnemy();
     }
 
+
     private void MoveEnemy()
     {
+        float horizontalMovement = movingRight ? 1f : -1f;
+        Vector2 movement = new Vector2(horizontalMovement, 0f) * moveSpeed * Time.deltaTime;
 
-        // moving the gameObject's position towards the bPos at the specified speed
-        Vector2 newPosition = Vector2.MoveTowards(transform.position, bPos, moveSpeed * Time.deltaTime);
 
-        // Smoothly moving the Rigidbody of the gameObject to the newPosition
-        GetComponent<Rigidbody2D>().MovePosition(newPosition);
+        transform.Translate(movement);
 
-        // converting Vector3 Transform's position into a Vector2 by using the (Vector2) type casting operation,
-        // the result is stored in a a Vector2 variable;
-        Vector2 position2D = (Vector2)transform.position;
 
-        // in Vector2, for comparing floating-point numbers we don't use direct equality (==) due to the potential for very small differences.
-        // so, I compare postion2D and bPos to see if they are very close to each other within a small threshold
-        if (Vector2.Distance(position2D, bPos) < 0.1f)
-        {       
-            if (bPos == aPos)
-            {
-                bPos = aPos + moveOffset;
-            }
-            else
-            {
-                bPos = aPos;
-            }
+        if (Mathf.Abs(transform.position.x - originalPosition.x) >= moveRange)
+        {
+            // Change direction when reaching the move range
+            movingRight = !movingRight;
         }
     }
 
