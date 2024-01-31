898e05d62d8e4e4aadceb0e4a8f1ee96bc801748
Atena Razm <atenarazm@gmail.com>
2024-01-30 17:34:47 -0800

    menu scipt get New Input System code added as a comment

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,9 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class Menu : MonoBehaviour
 {
+    // void Awake()
+    // {
+        //  // Instantiate the actions wrapper class
+        // playerActionControls = new PlayerActionControls();
+        // _rigidbody = GetComponent<Rigidbody2D>();
+
+        // // For the Play & Quit actions, adding a callback method for the "performed" phase
+        // menuControl.Menu.Play.performed += OnPlay;
+
+        // menuControl.Menu.Quit.performed += OnQuit;
+    // }
+    
+    
+    // private void OnPlay(InputAction.CallbackContext context)
+    // {
+    //     // This is the "Play" action callback method
+    //     SceneManager.LoadScene(2);
+    // }
+
+    // private void OnQuit(InputAction.CallbackContext context)
+    // {
+    //     // This is the "Quit" action callback method
+    //     SceneManager.LoadScene(0);
+    // }
+
     public void OnPlayButton()
     {
         SceneManager.LoadScene(2);
